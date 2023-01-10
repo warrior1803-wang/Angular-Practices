85d6d28977aa913f3d2a72e48cfd926c82535506
warrior1803-wang <60429051+warrior1803-wang@users.noreply.github.com>
2023-01-10 22:49:03 +0800

    observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selectValue = 'Recipes';
-  onNavigate(select: string) {
-    this.selectValue = select;
-  }
 }
